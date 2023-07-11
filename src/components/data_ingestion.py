5f6dd89bf99a074d6784c18d6e9552d9e4a88ef9
pawanjoshi2709 <pawanjoshi2709@gmail.com>
2023-07-11 22:29:45 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,53 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+##intialize the data Ingestion configration
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
+
+##create the data ingestion class
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info('data ingestion method start')
+
+        try:
+            df=pd.read_csv(os.path.join('notebooks/data','gemstone.csv'))#read from where you want example mongo db ,sql ,excel etc 
+            logging.info('dataset read as pandas dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+
+            logging.info('Raw data is created')
+
+            train_set,test_set=train_test_split(df,test_size=0.30,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            logging.info('train data is created')
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('test data is created')
+            logging.info('Ingestion of data is completed')
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+
+
+        except Exception as e :
+            logging.info('Exception occured at data ingestion stage')
+            raise CustomException(e,sys)
